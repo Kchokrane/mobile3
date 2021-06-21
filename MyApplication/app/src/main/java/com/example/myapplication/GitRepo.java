7a56f0af5e509234f336f8971e165e2779b76763
kchokrane <kchokrane@gmail.com>
2021-06-21 15:09:56 +0100

    push

---
+++ MyApplication/app/src/main/java/com/example/myapplication/GitRepo.java
@@ -0,0 +1,8 @@
+package com.example.myapplication;
+
+public class GitRepo {
+    public int id;
+    public String name;
+    public int size;
+    public String language;
+}
---
+++ MyApplication/app/src/main/java/com/example/myapplication/service/GitRepoServiceAPI.java
@@ -0,0 +1,17 @@
+package com.example.myapplication.service;
+
+import com.example.myapplication.GitRepo;
+import com.example.myapplication.GitUsersResponse;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+import java.util.List;
+
+public interface GitRepoServiceAPI {
+    @GET("search/users")
+    public Call<GitUsersResponse> searchUsers(@Query("q") String query);
+    @GET("users/{u}/repos")
+    public Call<List<GitRepo>> userRepositories(@Path("u") String login);
+}
